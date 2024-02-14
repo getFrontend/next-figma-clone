8a9d1e903f32482abaa8f7d9d347915c834cbf98
Sergey UP <sergey10goals@gmail.com>
2024-02-14 15:44:39 +0200

    added ScreenFitText text component which fit text to container

---
+++ components/ScreenFitText.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+
+const ScreenFitText = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const textRef = useRef<HTMLSpanElement | null>(null);
+
+  useEffect(() => {
+    resizeText();
+
+    window.addEventListener("resize", resizeText);
+
+    return () => {
+      window.removeEventListener("resize", resizeText);
+    };
+  }, []);
+
+  const resizeText = () => {
+    const container = containerRef.current;
+    const text = textRef.current;
+
+    if (!container || !text) {
+      return;
+    }
+
+    const containerWidth = container.offsetWidth;
+    let min = 1;
+    let max = 2500;
+
+    while (min <= max) {
+      const mid = Math.floor((min + max) / 2);
+      text.style.fontSize = mid + "px";
+
+      if (text.offsetWidth <= containerWidth) {
+        min = mid + 1;
+      } else {
+        max = mid - 1;
+      }
+    }
+
+    text.style.fontSize = max + "px";
+  };
+
+  return (
+    <div
+      className="z-0 flex h-screen w-full items-center overflow-hidden"
+      ref={containerRef}
+    >
+      <span
+        className="absolute bottom-0 left-0 mx-auto whitespace-nowrap text-center font-bold uppercase text-slate-700 pointer-events-none"
+        ref={textRef}
+      >
+        FIGMAN CREATE & SHARE
+      </span>
+    </div>
+  );
+};
+
+export default ScreenFitText;
