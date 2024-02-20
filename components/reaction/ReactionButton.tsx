ee5b02080c669b4eb3ed10936005ebaa9a397618
Sergey UP <sergey10goals@gmail.com>
2024-02-20 23:35:46 +0200

    updated from LiveBlocks documentation & Live Cursors Chat example

--- components/reaction/ReactionButton.tsx
+++ components/reaction/ReactionButton.tsx
@@ -1,7 +1,44 @@
-const ReactionButton = () => {
-  return (
-    <div>ReactionButton</div>
-  )
+import React from "react";
+
+type Props = {
+  setReaction: (reaction: string) => void;
 };
 
-export default ReactionButton;
+export default function ReactionSelector({ setReaction }: Props) {
+  return (
+    <div
+      className="-translate-x-1/2 -translate-y-1/2 transform rounded-full bg-white px-2"
+      style={{
+        boxShadow:
+          "0 0 0 0.5px rgba(0, 0, 0, 0.08), 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
+      }}
+      onPointerMove={(e) => e.stopPropagation()}
+    >
+      <ReactionButton reaction="👍" onSelect={setReaction} />
+      <ReactionButton reaction="🔥" onSelect={setReaction} />
+      <ReactionButton reaction="😍" onSelect={setReaction} />
+      <ReactionButton reaction="👀" onSelect={setReaction} />
+      <ReactionButton reaction="😱" onSelect={setReaction} />
+      <ReactionButton reaction="🙁" onSelect={setReaction} />
+    </div>
+  );
+}
+
+function ReactionButton(
+  {
+    reaction,
+    onSelect,
+  }: {
+    reaction: string;
+    onSelect: (reaction: string) => void;
+  }
+) {
+  return (
+    <button
+      className="transform select-none p-2 text-xl transition-transform hover:scale-150 focus:scale-150 focus:outline-none"
+      onPointerDown={() => onSelect(reaction)}
+    >
+      {reaction}
+    </button>
+  );
+}
