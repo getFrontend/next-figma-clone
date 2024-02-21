d042b7c90565612612d87e0bad2381483c036a56
Sergey UP <sergey10goals@gmail.com>
2024-02-21 23:17:07 +0200

    Implemented some Navbar functionality: added  undo & redo function via useUndo & useRedo hooks; added canvasObjects, syncShapeInStorage, deleteAllShapes, deleteShapeFromStorage; added actions on some mouse Events to work with canvas elements; added props to Navbar components: activeElement & handleActiveElement; added props to LeftSidebar component: allShapes

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,62 @@ import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSidebar from "@/components/RightSidebar";
 import ScreenFitText from "@/components/ScreenFitText";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { fabric } from "fabric";
-import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
+import { handleCanvasMouseDown, handleCanvasMouseUp, handleCanvasMouseMove, handleResize, initializeFabric, renderCanvas, handleCanvasObjectModified } from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
+import { useMutation, useRedo, useStorage, useUndo } from "@/liveblocks.config";
+import { defaultNavElement } from "@/constants";
+import { handleDelete, handleKeyDown } from "@/lib/key-events";
 
 export default function Page() {
+  // useUndo and useRedo are hooks provided by Liveblocks that allow you to undo and redo mutations.
+  const undo = useUndo();
+  const redo = useRedo();
+
+  /**
+     * useStorage is a hook provided by Liveblocks that allows you to storendata in a key-value store and automatically sync it with other users
+     * useStorage: https://liveblocks.io/docs/api-reference/liveblocks-react#useStorage
+     */
+  const canvasObjects = useStorage((root) => root.canvasObjects);
+
+  // syncShapeInStorage is a mutation that syncs the shape in the key-value tore of liveblocks
+  const syncShapeInStorage = useMutation(({ storage }, object) => {
+    // if the passed object is null, return
+    if (!object) return;
+
+    const { objectId } = object;
+    // Turn Fabric object (kclass) into JSON format so that we can store it in the key-value store
+    const shapeData = object.toJSON();
+    shapeData.objectId = objectId;
+
+    const canvasObjects = storage.get("canvasObjects");
+    // set is a method provided by Liveblocks that allows you to set a value
+    canvasObjects.set(objectId, shapeData);
+  }, []);
+
+  // deleteAllShapes is a mutation that deletes all the shapes from the key-value store
+  const deleteAllShapes = useMutation(({ storage }) => {
+    // get the canvasObjects store
+    const canvasObjects = storage.get("canvasObjects");
+
+    // if the store doesn't exist or is empty, return
+    if (!canvasObjects || canvasObjects.size === 0) return true;
+
+    // delete all the shapes from the store
+    for (const [key, value] of canvasObjects.entries()) {
+      canvasObjects.delete(key);
+    }
+
+    // return true if the store is empty
+    return canvasObjects.size === 0;
+  }, []);
+
+  const deleteShapeFromStorage = useMutation(({ storage }, shapeId) => {
+    const canvasObjects = storage.get("canvasObjects");
+    canvasObjects.delete(shapeId);
+  }, []);
+
   // canvasRef is a reference to the canvas element that we'll use to initialize the fabric canvas.
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -30,20 +81,51 @@ export default function Page() {
   const shapeRef = useRef<fabric.Object | null>(null);
 
   // selectedShapeRef is a reference to the shape that the user has selected.
-  const selectedShapeRef = useRef<string | null>("rectangle");
+  const selectedShapeRef = useRef<string | null>("null");
+
+  const activeObjectRef = useRef<fabric.Object | null>(null);
+
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: "",
+    value: "",
+    icon: ""
+  });
+
+
+  const handleActiveElement = (elem: ActiveElement) => {
+    setActiveElement(elem);
+
+    switch (elem?.value) {
+      // delete all the shapes from the canvas
+      case "reset":
+        // clear the storage
+        deleteAllShapes();
+        // clear the canvas
+        fabricRef.current?.clear();
+        // set "select" as the active element
+        setActiveElement(defaultNavElement);
+        break;
+      case "delete":
+        // delete it from the canvas
+        handleDelete(fabricRef.current as any, deleteShapeFromStorage);
+        // set "select" as the active element
+        setActiveElement(defaultNavElement);
+        break;
+    }
+
+    selectedShapeRef.current = elem?.value as string;
+  };
 
   useEffect(() => {
     // initialize the fabric canvas
     const canvas = initializeFabric({ canvasRef, fabricRef });
 
     /**
-     * listen to the mouse down event on the canvas which is fired when the
-     * user clicks on the canvas
-     *
+     * listen to the mouse down event on the canvas which is fired when the user clicks on the canvas
      * Event inspector: http://fabricjs.com/events
      * Event list: http://fabricjs.com/docs/fabric.Canvas.html#fire
      */
-    canvas.on("mouse:down", (options) => {
+    canvas.on("mouse:down", (options: any) => {
       handleCanvasMouseDown({
         options,
         canvas,
@@ -53,19 +135,77 @@ export default function Page() {
       });
     });
 
+    canvas.on("mouse:move", (options: any) => {
+      handleCanvasMouseMove({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage
+      });
+    });
+
+    canvas.on("mouse:up", (options: any) => {
+      handleCanvasMouseUp({
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage,
+        setActiveElement,
+        activeObjectRef
+      });
+    });
+
+    canvas.on("object:modified", (options: any) => {
+      handleCanvasObjectModified({
+        options,
+        syncShapeInStorage,
+      });
+    });
+
     window.addEventListener("resize", () => {
       handleResize({
         canvas: fabricRef.current,
       });
     });
+
+    window.addEventListener("keydown", (e: any) =>
+      handleKeyDown({
+        e,
+        canvas: fabricRef.current,
+        undo,
+        redo,
+        syncShapeInStorage,
+        deleteShapeFromStorage,
+      })
+    );
+
+    // dispose the canvas and remove the event listeners when the component unmounts
+    return () => {
+      canvas.dispose();
+    };
   }, []);
+
+  // render the canvas when the canvasObjects from live storage changes
+  useEffect(() => {
+    renderCanvas({
+      fabricRef,
+      canvasObjects,
+      activeObjectRef,
+    });
+  }, [canvasObjects]);
 
   return (
     <main className="h-screen overflow-hidden">
-      <Navbar />
+      <Navbar
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}
+      />
       <section className="flex flex-row h-full">
         <h1 className="sr-only text-white">Figman - a minimalist clone of Figma</h1>
-        <LeftSidebar />
+        <LeftSidebar allShapes={Array.from(canvasObjects)} />
         <Live canvasRef={canvasRef} />
         <RightSidebar />
       </section>
