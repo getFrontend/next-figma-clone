9cc6c058efd11285f92c747b5ad18d19bd08ad3d
Sergey UP <sergey10goals@gmail.com>
2024-02-21 13:08:22 +0200

    Implemented Live Reactions according to LiveBlocks documentation

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,21 +1,36 @@
-import { useMyPresence, useOthers } from "@/liveblocks.config";
+import { useBroadcastEvent, useEventListener, useMyPresence, useOthers } from "@/liveblocks.config";
 import LiveCursors from "./cursor/LiveCursors";
-import { CursorMode, Reaction } from "@/types/type";
+import { CursorMode, CursorState, Reaction, ReactionEvent } from "@/types/type";
 import { useCallback, useEffect, useState } from "react";
 import ScreenFitText from "./ScreenFitText";
 import CursorChat from "./cursor/CursorChat";
 import ReactionSelector from "./reaction/ReactionButton";
+import FlyingReaction from "./reaction/FlyingReaction";
+import useInterval from "@/hooks/useInterval";
 
 const Live = () => {
   // useOthers returns the list of other users in the room.
   const others = useOthers();
+
+  /** useMyPresence returns the presence of the current user in the room.
+  * It also returns a function to update the presence of the current user.
+  * useMyPresence: https://liveblocks.io/docs/api-reference/liveblocks-react#useMyPresence
+  */
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
 
-  const [cursorState, setCursorState] = useState({
-    mode: CursorMode.Hidden
+  // track the state of the cursor (hidden, chat, reaction, reaction selector)
+  const [cursorState, setCursorState] = useState<CursorState>({
+    mode: CursorMode.Hidden,
   });
 
+  // store the reactions created on mouse click
   const [reactions, setReactions] = useState<Reaction[]>([]);
+
+  /**
+  * useBroadcastEvent is used to broadcast an event to all the other users in the room.
+  * useBroadcastEvent: https://liveblocks.io/docs/api-reference/liveblocks-react#useBroadcastEvent
+  */
+  const broadcast = useBroadcastEvent();
 
   // Listen to keyboard events to change the cursor state
   useEffect(() => {
@@ -49,6 +64,51 @@ const Live = () => {
     };
   }, [updateMyPresence]);
 
+  // Remove reactions that are not visible anymore (every 1 sec)
+  useInterval(() => {
+    setReactions((reactions) => reactions.filter((reaction) => reaction.timestamp > Date.now() - 3000));
+  }, 1000);
+
+  // Broadcast the reaction to other users (every 100ms)
+  useInterval(() => {
+    if (cursorState.mode === CursorMode.Reaction && cursorState.isPressed && cursor) {
+      // concat all the reactions created on mouse click
+      setReactions((reactions) =>
+        reactions.concat([
+          {
+            point: { x: cursor.x, y: cursor.y },
+            value: cursorState.reaction,
+            timestamp: Date.now(),
+          },
+        ])
+      );
+
+      // Broadcast the reaction to other users
+      broadcast({
+        x: cursor.x,
+        y: cursor.y,
+        value: cursorState.reaction,
+      });
+    };
+  }, 100);
+
+  /**
+   * useEventListener is used to listen to events broadcasted by other users.
+   * useEventListener: https://liveblocks.io/docs/api-reference/liveblocks-react#useEventListener
+   */
+  useEventListener((eventData) => {
+    const event = eventData.event as ReactionEvent;
+    setReactions((reactions) =>
+      reactions.concat([
+        {
+          point: { x: event.x, y: event.y },
+          value: event.value,
+          timestamp: Date.now(),
+        },
+      ])
+    );
+  });
+
   // Listen to mouse events to change the cursor state
   const handlePointerMove = useCallback((event: React.PointerEvent) => {
     event.preventDefault();
@@ -74,9 +134,13 @@ const Live = () => {
     });
   }, []);
 
-  const handlePointerUp = () => {
-
-  }
+  const handlePointerUp = useCallback((event: React.PointerEvent) => {
+    setCursorState((state: CursorState) =>
+      cursorState.mode === CursorMode.Reaction ?
+        { ...state, isPressed: false } :
+        state
+    );
+  }, [cursorState.mode, setCursorState]);
 
   const handlePointerDown = useCallback((event: React.PointerEvent) => {
     const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
@@ -84,10 +148,18 @@ const Live = () => {
 
     updateMyPresence({ cursor: { x, y } });
 
-    setCursorState((state: CursorState) => {
-      cursorState.mode === CursorMode.Reaction ?
-        { ...state, isPressed: true } :
-        state
+    // if cursor is in reaction mode, set isPressed to true
+    setCursorState((state: CursorState) =>
+      cursorState.mode === CursorMode.Reaction ? { ...state, isPressed: true } : state
+    );
+  }, [cursorState.mode, setCursorState]);
+
+  // set the reaction of the cursor
+  const setReaction = useCallback((reaction: string) => {
+    setCursorState({
+      mode: CursorMode.Reaction,
+      reaction,
+      isPressed: false
     });
   }, []);
 
@@ -96,10 +168,21 @@ const Live = () => {
       onPointerMove={handlePointerMove}
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
-    // onPointerUp={handlePointerUp}
+      onPointerUp={handlePointerUp}
     >
       <h1 className="sr-only text-white">Figman - a minimalist clone of Figma</h1>
       <ScreenFitText />
+
+      {reactions.map((reaction) => (
+        <FlyingReaction
+          key={reaction.timestamp.toString()}
+          x={reaction.point.x}
+          y={reaction.point.y}
+          timestamp={reaction.timestamp}
+          value={reaction.value}
+        />
+      )
+      )}
 
       {/* If cursor is in chat mode, show the chat cursor */}
       {cursor && (
@@ -114,9 +197,8 @@ const Live = () => {
       {cursorState.mode === CursorMode.ReactionSelector && (
         <ReactionSelector
           setReaction={(reaction) => {
-            setReactions(reaction);
+            setReaction(reaction);
           }}
-
         />
       )}
 
--- components/reaction/ReactionButton.tsx
+++ components/reaction/ReactionButton.tsx
@@ -7,19 +7,17 @@ type Props = {
 export default function ReactionSelector({ setReaction }: Props) {
   return (
     <div
-      className="-translate-x-1/2 -translate-y-1/2 transform rounded-full bg-white px-2"
-      style={{
-        boxShadow:
-          "0 0 0 0.5px rgba(0, 0, 0, 0.08), 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
-      }}
+      className="absolute bottom-20 left-0 right-0 mx-auto w-fit transform rounded-full bg-white px-2"
       onPointerMove={(e) => e.stopPropagation()}
     >
       <ReactionButton reaction="👍" onSelect={setReaction} />
       <ReactionButton reaction="🔥" onSelect={setReaction} />
       <ReactionButton reaction="😍" onSelect={setReaction} />
       <ReactionButton reaction="👀" onSelect={setReaction} />
+      <ReactionButton reaction="😎" onSelect={setReaction} />
       <ReactionButton reaction="😱" onSelect={setReaction} />
       <ReactionButton reaction="🙁" onSelect={setReaction} />
+      <ReactionButton reaction="😂" onSelect={setReaction} />
     </div>
   );
 }
