7b4b281ff2747f7a9db350a333fb266d023ffd82
Sergey UP <sergey10goals@gmail.com>
2024-02-21 13:40:17 +0200

    added generateRandomName function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,46 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const adjectives = [
+  "Happy",
+  "Creative",
+  "Energetic",
+  "Lively",
+  "Dynamic",
+  "Radiant",
+  "Joyful",
+  "Vibrant",
+  "Cheerful",
+  "Sunny",
+  "Sparkling",
+  "Bright",
+  "Shining",
+];
+
+const animals = [
+  "Dolphin",
+  "Tiger",
+  "Elephant",
+  "Penguin",
+  "Kangaroo",
+  "Panther",
+  "Lion",
+  "Cheetah",
+  "Giraffe",
+  "Hippopotamus",
+  "Monkey",
+  "Panda",
+  "Crocodile",
+];
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+  return twMerge(clsx(inputs));
+};
+
+export function generateRandomName(): string {
+  const randomAdjective =
+    adjectives[Math.floor(Math.random() * adjectives.length)];
+  const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
+
+  return `${randomAdjective} ${randomAnimal}`;
+};
