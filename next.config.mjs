da9fb2343c4fe1897abcb7c00dcf9d7c32739d7e
Sergey UP <sergey10goals@gmail.com>
2024-02-22 00:49:38 +0200

    tried to fix an error while deploying to Vercel (3)

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
       bufferutil: "commonjs bufferutil",
-      canvas: "https://registry.yarnpkg.com/@favware/skip-dependency/-/skip-dependency-1.2.1.tgz"
+      canvas: "commonjs canvas",
     });
     return config;
   },
