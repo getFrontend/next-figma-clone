c789dc495ff52b58aacaf05c0ea8daa5276910ba
Sergey UP <sergey10goals@gmail.com>
2024-02-22 13:21:32 +0200

    updated Loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 
 const Loader = () => (
-  <div className='flex h-screen w-screen flex-col items-center justify-center gap-2'>
+  <div className="flex h-screen w-screen flex-col items-center justify-center gap-2">
     <Image
-      src='/assets/loader.gif'
-      alt='loader'
-      width={100}
-      height={100}
-      className='object-contain'
+      src="/assets/loader.svg"
+      alt="loader"
+      width={200}
+      height={200}
+      className="object-contain"
     />
-    <p className='text-sm font-bold text-primary-grey-300'>Loading...</p>
+    <p className="mt-4 text-lg font-bold text-primary-grey-300 tracking-widest">Loading...</p>
   </div>
 );
 
