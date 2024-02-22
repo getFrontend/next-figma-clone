423d3606edfc5c24c04856ac62dec93e121caf16
Sergey UP <sergey10goals@gmail.com>
2024-02-22 14:23:21 +0200

    refactoring: removed unused code

--- components/settings/Color.tsx
+++ components/settings/Color.tsx
@@ -28,9 +28,6 @@ const Color = ({
         onChange={(e) => handleInputChange(attributeType, e.target.value)}
       />
       <Label className='flex-1'>{attribute}</Label>
-      <Label className='flex h-6 w-8 items-center justify-center bg-primary-grey-100 text-[10px] leading-3'>
-        90%
-      </Label>
     </div>
   </div>
 );
