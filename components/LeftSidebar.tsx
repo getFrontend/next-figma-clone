c006f0d79474359f8d031e4c63ae56d8ca106ca3
Sergey UP <sergey10goals@gmail.com>
2024-02-23 08:56:12 +0200

    added copyright info

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -11,26 +11,32 @@ const LeftSidebar = ({ allShapes }: { allShapes: Array<any> }) => {
     () => (
       <section className=" h-full sticky left-0 pb-20 flex flex-col select-none overflow-y-auto border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 max-lg:hidden min-w-[180px]">
         <h2 className="border border-primary-grey-200 px-5 py-4 text-xs uppercase">Layers</h2>
-        <div className="flex flex-col">
-          {allShapes?.map((shape: any) => {
-            const info = getShapeInfo(shape[1]?.type);
+        <div className="flex flex-col flex-1">
+          <div className="flex flex-col flex-1">
+            {allShapes?.map((shape: any) => {
+              const info = getShapeInfo(shape[1]?.type);
 
-            return (
-              <div
-                key={shape[1]?.objectId}
-                className="group my-1 flex items-center gap-2 px-5 py-2.5 hover:cursor-pointer hover:bg-primary-green hover:text-primary-black"
-              >
-                <Image
-                  src={info?.icon}
-                  alt='Layer'
-                  width={16}
-                  height={16}
-                  className='group-hover:invert'
-                />
-                <h3 className='text-sm font-semibold capitalize'>{info.name}</h3>
-              </div>
-            );
-          })}
+              return (
+                <div
+                  key={shape[1]?.objectId}
+                  className="group my-1 flex items-center gap-2 px-5 py-2.5 hover:cursor-pointer hover:bg-primary-green hover:text-primary-black"
+                >
+                  <Image
+                    src={info?.icon}
+                    alt='Layer'
+                    width={16}
+                    height={16}
+                    className='group-hover:invert'
+                  />
+                  <h3 className='text-sm font-semibold capitalize'>{info.name}</h3>
+                </div>
+              );
+            })}
+          </div>
+          <div className="text-sm flex flex-col px-3 py-4 gap-2 self-center">
+            <p>Developer: Sergey UP</p>
+            <p className="text-primary-green">© 2024, FigMan</p>
+          </div>
         </div>
       </section>
     ),
